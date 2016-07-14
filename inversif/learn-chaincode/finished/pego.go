59559247cb10ae740da04539b23db1d988abca34
dionisius <dion.alfaputra1@gmail.com>
2016-07-14 11:57:55 +0700

    Add debug code for putback function

--- inversif/learn-chaincode/finished/pego.go
+++ inversif/learn-chaincode/finished/pego.go
@@ -162,6 +162,8 @@ func AssignToEmployee(id string, name string, title string, level string,
 func PutBack(stub *shim.ChaincodeStub, employee Member, key int) ([]byte, error) {
 	employeeAsBytes, _ := json.Marshal(employee)
 
+	fmt.Println("Employee key CC: ", key, " data ", employeeAsBytes)
+
 	strkey := strconv.Itoa(key)
 	err := stub.PutState(strkey, employeeAsBytes) //write the new employee to the chaincode state
 	if err != nil {
