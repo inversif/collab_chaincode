a659de35710ed09b7f131420cb0feec78f1d6823
Yosua Witantra <yosuawitantra@gmail.com>
2016-07-14 13:17:15 +0700

    changed read function

--- inversif/learn-chaincode/finished/pego.go
+++ inversif/learn-chaincode/finished/pego.go
@@ -113,7 +113,7 @@ func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte,
 
 	key = args[0]
 	valAsbytes, err := stub.GetState(key)
-	if err != nil {
+	if valAsbytes != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
