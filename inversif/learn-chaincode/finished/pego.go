1d88c06d9ddf68fdaa502883198eb4c2ceecc7de
Yosua Witantra <yosuawitantra@gmail.com>
2016-07-14 13:35:34 +0700

    Got rid of unused variable

--- inversif/learn-chaincode/finished/pego.go
+++ inversif/learn-chaincode/finished/pego.go
@@ -105,14 +105,13 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 // read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var key, jsonResp string
-	var err error
 
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
 	key = args[0]
-	valAsbytes, err := stub.GetState(key)
+	valAsbytes, _ := stub.GetState(key)
 	if valAsbytes != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
